271c5c8e041cfbdceb14e4db4fe50a9f9715ae4a
max-devisser <maxdevisser@umail.ucsb.edu>
2017-02-03 19:30:44 -0800

    Update SearchHandler.java

--- SearchHandler.java
+++ SearchHandler.java
@@ -1,3 +1,7 @@
 // Search through a MovieCollection given a query
 // Pass in a MovieCollection and a query
 // Return a smaller MovieCollection of Movies that have terms that match the query
+
+public static MovieCollection search(MovieCollection inputCollection, String query) {
+ 
+}
