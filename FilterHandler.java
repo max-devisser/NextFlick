8dffdd6620770028bb5dd1380534b34f3603bfbd
max-devisser <maxdevisser@umail.ucsb.edu>
2017-02-03 19:43:52 -0800

    Update FilterHandler.java

--- FilterHandler.java
+++ FilterHandler.java
@@ -1,3 +1,7 @@
 // Filter a movie collection based on a parameter
 // Pass in a MovieCollection and a parameter
 // Returns smaller MovieCollection of only Movies that match the parameter
+
+public static MovieCollection filter(MovieCollection inputCollection, String parameter) {
+  
+}
