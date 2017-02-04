bf934224c5fba75c8a72c91b71ef848fb4d52eb2
bkorycki97 <bkorycki@umail.ucsb.edu>
2017-02-03 19:15:01 -0800

    Create SearchController.java

---
+++ SearchController.java
@@ -0,0 +1,15 @@
+public class SearchController{
+  SearchGUI gui;
+  //constructor
+  public SearchController(){
+  
+  }
+  public void go(SearchGUI g){
+    gui = g;
+    //call gui functions to set up gui
+    //wait for user input
+  }
+  public void quit(){
+  
+  }
+}
