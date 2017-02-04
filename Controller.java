2556a4d7078daa596f372da628fd52fe8964f28b
bkorycki97 <bkorycki@umail.ucsb.edu>
2017-02-03 19:11:05 -0800

    Update

--- Controller.java
+++ Controller.java
@@ -3,12 +3,13 @@
 public class Controller{
   HomeGUI homeGUI;
   public void go(){
-    homeGui = new HomeGUI();
+    homeGUI = new HomeGUI();
     //check what user wants to do
     //Iteration 1: automatically open search page
     SearchGUI searchGui = new SearchGUI();
-    searchGUI.go();
-    searchGUI.quit();
+    SearchController searchController = new SearchController();
+    searchController.go(searchGUI);
+    searchController.quit();
   }
   public static void main(String[] args){
     Controller controller = new Controller();
