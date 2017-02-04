5150339231b3172ada5bade3b75d6a88e87dc306
max-devisser <maxdevisser@umail.ucsb.edu>
2017-02-03 19:37:06 -0800

    Update SortHandler.java

--- SortHandler.java
+++ SortHandler.java
@@ -1,4 +1,8 @@
 // Sort a movie collection based on a parameter
 // Pass in a MovieCollection and a sort parameter (check to make sure that what they want to sort by makes sense, 
-   e.g. don't sort by plot or posterURL)
+//   e.g. don't sort by plot or posterURL)
 // Returns sorted MovieCollection
+
+public static MovieCollection sort(MovieCollection inputCollection, String parameter) {
+   
+}
