3f54e7feb01b67a6990bad10a11e6d9c0bd98ca2
max-devisser <maxdevisser@umail.ucsb.edu>
2017-02-03 19:04:16 -0800

    Create Movie.java

---
+++ Movie.java
@@ -0,0 +1,5 @@
+// Stores info on movies
+// Have constructors to initialize data
+// Have getters for all information
+// String title, int year, String director, String genre, String[] actors, String parentalRating, String runtime, String language, 
+   String country, double IMdBRating, String plot, String imageURL
