7e337831ad30308c095780399cf8249cf85da30d
max-devisser <maxdevisser@umail.ucsb.edu>
2017-02-03 19:07:48 -0800

    Update MovieCollection.java

--- MovieCollection.java
+++ MovieCollection.java
@@ -1,3 +1,4 @@
 // Container for Movie objects
 // Getters/setters for Movie objects
 // Be able to remove Movie objects
+// Maybe make it an ArrayList with an overwritten .equals method to let you match movies based on only one parameter matching
