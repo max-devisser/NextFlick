32d375e9aeef126285f2e7a3d07317146151444a
cmargerum <cole.margerum@gmail.com>
2017-02-21 20:48:01 -0800

    Delete SearchHandler.java

--- src/SearchHandler.java
+++
@@ -1,5 +0,0 @@
-package src;
-// Search through a MovieCollection given a query
-// Pass in a MovieCollection and a query
-// Return a smaller MovieCollection of Movies that have terms that match the query
-
