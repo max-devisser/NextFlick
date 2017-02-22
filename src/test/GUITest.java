4629f31a4e9e6655e46a334e58cc4ab779df739f
Max de Visser <maxdevisser@umail.ucsb.edu>
2017-02-21 20:20:06 -0800

    Added JUnit tests for main classes (not movie builder)

---
+++ src/test/GUITest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package src.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author max
+ *
+ */
+public class GUITest {
+
+	/**
+	 * Test method for {@link src.GUI#GUI()}.
+	 */
+	@Test
+	public void testGUI() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#go()}.
+	 */
+	@Test
+	public void testGo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#setResults(java.util.HashMap)}.
+	 */
+	@Test
+	public void testSetResults() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#getSearch()}.
+	 */
+	@Test
+	public void testGetSearch() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#getFilter()}.
+	 */
+	@Test
+	public void testGetFilter() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#getSort()}.
+	 */
+	@Test
+	public void testGetSort() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#quit()}.
+	 */
+	@Test
+	public void testQuit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link src.GUI#tabClick()}.
+	 */
+	@Test
+	public void testTabClick() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/HomePanelTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package src.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author max
+ *
+ */
+public class HomePanelTest {
+
+	/**
+	 * Test method for {@link src.HomePanel#paintComponent(java.awt.Graphics)}.
+	 */
+	@Test
+	public void testPaintComponentGraphics() {
+		fail("Not yet implemented");
+	}
+
+}
