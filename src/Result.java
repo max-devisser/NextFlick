cce8a267ffafd456a2bbf15a2955561bc765aeb3
bkorycki97 <bkorycki@umail.ucsb.edu>
2017-02-22 21:56:45 -0800

    Update Result.java

--- src/Result.java
+++ src/Result.java
@@ -1,1 +1,22 @@
+package src;
+import javax.swing.*;
+import java.awt.*;
 
+public class Result extends JPanel{
+	private Movie movie;
+	private JLabel title;
+	public Result(Movie movie){
+		movie = movie;
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setMinimumSize(new Dimension(Integer.MAX_VALUE, 300));	
+
+
+		this.add(new JLabel(movie.toString()));
+		this.add(new JLabel(movie.getDate()));
+
+		this.setOpaque(true);
+		this.setBackground(Color.BLUE);
+		this.setForeground(Color.RED);
+
+	}
+}
