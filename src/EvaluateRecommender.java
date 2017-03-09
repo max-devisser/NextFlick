0e2f158a03abf23ab4f94fb29212d5a28d95b28b
Max de Visser <maxdevisser@umail.ucsb.edu>
2017-03-09 03:57:01 -0800

    Basic sample recommender with sample dataset

---
+++ src/EvaluateRecommender.java
@@ -0,0 +1,44 @@
+package src;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
+import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class EvaluateRecommender {
+
+	public static void main(String[] args) throws IOException, TasteException {
+		DataModel model = new FileDataModel(new File("res/dataset.csv"));
+		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
+		RecommenderBuilder builder = new MyRecommenderBuilder();
+		double result = evaluator.evaluate(builder, null, model, 0.9, 1.0); // Lower number is better 
+					// result. Takes a builder to make a recommendation based off the model, then trains
+					// the algorithm with 90% of the data, then checks how well it will do on the
+					// entirety of the data. Works better/more consistently with larger datasets
+		System.out.println(result);
+	}
+	
+}
+
+// helper class to build a recommendation model
+class MyRecommenderBuilder implements RecommenderBuilder {
+
+	@Override
+	public Recommender buildRecommender(DataModel dataModel) throws TasteException {
+		UserSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+		UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, dataModel);
+		return new GenericUserBasedRecommender(dataModel, neighborhood, similarity);
+	}
+	
+}
