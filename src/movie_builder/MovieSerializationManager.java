0a5e96fdfb498236663dcf76cb7eec8e999d2ff5
Max de Visser <maxdevisser@umail.ucsb.edu>
2017-03-08 22:47:20 -0800

    fix dependencies

--- src/movie_builder/MovieSerializationManager.java
+++ src/movie_builder/MovieSerializationManager.java
@@ -19,6 +19,7 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
